e3842969b8184c54701abf269e9543d9f8c661c3
Rishi Barua <ribarua@microsoft.com>
2019-05-26 20:58:40 -0700

    Add Type1 validation tests

---
+++ SampleCode/TypeValidationTests/Type1Tests/VariationsInWhiteSpaceLayoutsComments.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+public class Type1Tests
+{
+	public int OriginalDoubledSumFunction()
+	{
+        int sum = 0;
+
+        for(int i = 0; i < 100; i++)
+        {
+            sum = sum + i;
+        }
+
+        int doubledSum = sum * 2;
+        return doubledSum;
+	}
+
+    public int DoubledSumFunctionWithWhitespace()
+    {
+        int sum = 0;
+
+
+
+
+
+        for (int i = 0; i < 100; i++)
+        {
+            sum =       sum + i;
+        }
+
+
+
+
+        int doubledSum = sum * 2;
+        return doubledSum;
+    }
+
+    public int DoubledSumFunctionWithComments()
+    {
+        int sum = 0;
+
+        // This is a for loop to calculate sum
+        for (int i = 0; i < 100; i++)
+        {
+            sum = sum + i;
+        }
+
+        // Sum is being doubled here
+        int doubledSum = sum * 2;
+        return doubledSum;
+    }
+
+    public int DoubledSumFunctionWithLayoutChange()
+    {
+        int sum = 0;
+        int doubledSum = sum * 2;
+
+        for (int i = 0; i < 100; i++)
+        {
+            sum = sum + i;
+        }
+
+        return doubledSum;
+    }
+}
---
+++ TypeValidationTests/TypeValidationTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CountMatrixCloneDetection;
+
+namespace TypeValidationTests
+{
+    [TestClass]
+    public class TypeValidationTests
+    {
+        [TestMethod]
+        public void ValidateType1Tests()
+        {
+            var currentPath = "../../../SampleCode/TypeValidationTests/Type1Tests/";
+
+            // Act
+            var cmcdResults = CMCD.Run(currentPath);
+
+            Assert.IsTrue(cmcdResults.Count == 6);
+
+            foreach (var result in cmcdResults)
+            {
+                Assert.IsTrue(result.Score == 0, string.Format("Test failed for {0}, {1}. Expected Score = 0, Actual score = {2}",
+                    result.MethodA, result.MethodB, result.Score));
+            }
+        }
+    }
+}
